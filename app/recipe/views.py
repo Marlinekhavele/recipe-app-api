eb49b7903ecb76ec6cd648dd7d3701d8dc4797e4
MarlineKhavele <marlinekhavele6@gmail.com>
2020-01-09 19:03:55 +0300

    Recipe API done.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -18,10 +18,19 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
-        """Create a new ingredient"""
+        """Create a new object"""
         serializer.save(user=self.request.user)
 
 
@@ -44,9 +53,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
-    def_params_to_ints(self, qs):
-    """Convert alist of string IDs to a list of integers"""
-    return [int(str_id) for str_id in qs.split(',')]
+    def _params_to_ints(self, qs):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(',')]
 
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user"""
@@ -60,56 +69,15 @@ class RecipeViewSet(viewsets.ModelViewSet):
             ingredient_ids = self._params_to_ints(ingredients)
             queryset = queryset.filter(ingredients__id__in=ingredient_ids)
 
-        return self.queryset.filter(user=self.request.user)
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
-        elif self.action def test_filter_recipes_by_tags(self):
-        """Test returning recipes with specific tags"""
-        recipe1 = sample_recipe(user=self.user, title='Thai vegetable curry')
-        recipe2 = sample_recipe(user=self.user, title='Aubergine with tahini')
-        tag1 = sample_tag(user=self.user, name='Vegan')
-        tag2 = sample_tag(user=self.user, name='Vegetarian')
-        recipe1.tags.add(tag1)
-        recipe2.tags.add(tag2)
-        recipe3 = sample_recipe(user=self.user, title='Fish and chips')
-
-        res = self.client.get(
-            RECIPES_URL,
-            {'tags': f'{tag1.id},{tag2.id}'}
-        )
-
-        serializer1 = RecipeSerializer(recipe1)
-        serializer2 = RecipeSerializer(recipe2)
-        serializer3 = RecipeSerializer(recipe3)
-        self.assertIn(serializer1.data, res.data)
-        self.assertIn(serializer2.data, res.data)
-        self.assertNotIn(serializer3.data, res.data)
-
-    def test_filter_recipes_by_ingredients(self):
-        """Test returning recipes with specific ingredients"""
-        recipe1 = sample_recipe(user=self.user, title='Posh beans on toast')
-        recipe2 = sample_recipe(user=self.user, title='Chicken cacciatore')
-        ingredient1 = sample_ingredient(user=self.user, name='Feta cheese')
-        ingredient2 = sample_ingredient(user=self.user, name='Chicken')
-        recipe1.ingredients.add(ingredient1)
-        recipe2.ingredients.add(ingredient2)
-        recipe3 = sample_recipe(user=self.user, title='Steak and mushrooms')
-
-        res = self.client.get(
-            RECIPES_URL,
-            {'ingredients': f'{ingredient1.id},{ingredient2.id}'}
-        )
-
-        serializer1 = RecipeSerializer(recipe1)
-        serializer2 = RecipeSerializer(recipe2)
-        serializer3 = RecipeSerializer(recipe3)
-        self.assertIn(serializer1.data, res.data)
-        self.assertIn(serializer2.data, res.data)
-        self.assertNotIn(serializer3.data, res.data) == 'upload_image':
+        elif self.action == 'upload_image':
             return serializers.RecipeImageSerializer
+
         return self.serializer_class
 
     def perform_create(self, serializer):
@@ -120,8 +88,19 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def upload_image(self, request, pk=None):
         """Upload an image to a recipe"""
         recipe = self.get_object()
-        serializer = self.get_serializer(recipe, data=request.data)
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data
+        )
+
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
