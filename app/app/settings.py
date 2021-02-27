a2e14ce0b26600d546d888067a46e496eef8020b
MarlineKhavele <marlinekhavele6@gmail.com>
2021-02-28 02:13:30 +0300

    Added swaggere

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_swagger',
     'core',
     'user',
     'recipe',
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # for swagger
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = '/vol/web/media'
--- app/app/urls.py
+++ app/app/urls.py
@@ -17,7 +17,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+
+api_v1_docs = [
+    path('openapi', get_schema_view(
+        title="Recipe APP",
+        description="Recipe APIs",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    path('docs/',TemplateView.as_view(
+        template_name='swagger-docs.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui')
+]
 urlpatterns = [
+    path('api/v1/', include(api_v1_docs)),
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
